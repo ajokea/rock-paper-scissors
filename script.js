d16e7515ead75063f626857598b43dc01a0d1e41
ajoke-naje <ajokenakinseye@gmail.com>
2024-08-12 19:00:50 -0400

    Create function for playing a single round

--- script.js
+++ script.js
@@ -25,3 +25,47 @@ function getHumanChoice() {
 let humanScore = 0
 let computerScore = 0
 
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === computerChoice) {
+        console.log("Tie!")
+    } else {
+        if(humanChoice == 'rock') {
+            switch(computerChoice) {
+                case 'paper':
+                    computerScore++
+                    console.log("Computer wins! Paper beats rock!")
+                    break;
+                
+                case 'scissors':
+                    humanScore++
+                    console.log("You win! Rock beats scissors!")
+                    break;
+            }
+
+        } else if(humanChoice === 'paper') {
+            switch(computerChoice) {
+                case 'rock':
+                    humanScore++
+                    console.log("You win! Paper beats rock!")
+                    break;
+                
+                case 'scissors':
+                    computerScore++
+                    console.log("Computer wins! Scissors beats paper!")
+                    break;
+            }
+        } else {
+            switch(computerChoice) {
+                case 'rock':
+                    computerScore++
+                    console.log("Computer wins! Rock beats scissors!")
+                    break;
+                
+                case 'paper':
+                    humanScore++
+                    console.log("You win! Scissors beats paper!")
+                    break;
+            }
+        }
+    }
+}
